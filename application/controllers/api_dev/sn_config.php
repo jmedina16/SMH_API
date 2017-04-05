61d89131cd544972987494ad5815184102e09e43
root <jmedina616@gmail.com>
2017-04-05 05:30:30 +0000

    latest update

--- application/controllers/api_dev/sn_config.php
+++ application/controllers/api_dev/sn_config.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct scripts access allowed');
 
 require APPPATH . '/libraries/REST_Controller.php';
 
