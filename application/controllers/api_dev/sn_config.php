f1f5f84aed76852b391d0735f8087a82e7882a32
root <jmedina616@gmail.com>
2017-04-19 06:00:10 +0000

    latest update

--- application/controllers/api_dev/sn_config.php
+++ application/controllers/api_dev/sn_config.php
@@ -499,6 +499,7 @@ class Sn_config extends REST_Controller {
         $privacy = $this->get('privacy');
         $create_vod = $this->get('create_vod');
         $cont_streaming = $this->get('cont_streaming');
+        $projection = $this->get('projection');
 
         if (!isset($pid) || $pid == null) {
 
@@ -535,7 +536,12 @@ class Sn_config extends REST_Controller {
             $this->response(array('error' => 'Missing Cont Streaming'), 200);
         }
 
-        $result = $this->sn_config_model->create_fb_livestream($pid, $ks, $stream_to, $asset_id, $privacy, $create_vod, $cont_streaming);
+        if (!isset($projection) || $projection == null) {
+
+            $this->response(array('error' => 'Missing projection'), 200);
+        }
+
+        $result = $this->sn_config_model->create_fb_livestream($pid, $ks, $stream_to, $asset_id, $privacy, $create_vod, $cont_streaming, $projection);
 
         if (!$result) {
 
