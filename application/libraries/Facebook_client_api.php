050d8abc053a9de82fce8ae777a5a6371638f3c9
root <jmedina616@gmail.com>
2017-04-25 05:00:24 +0000

    latest update

--- application/libraries/Facebook_client_api.php
+++ application/libraries/Facebook_client_api.php
@@ -234,7 +234,7 @@ class Facebook_client_api {
             return $success;
         } catch (\Facebook\Exceptions\FacebookResponseException $e) {
             syslog(LOG_NOTICE, "SMH DEBUG : Graph returned an error: " . $e->getMessage());
-            $success = array('success' => false, 'message' => 'SMH app may not have permission to create live streams on this user');
+            $success = array('success' => false, 'message' => 'Does not have permission to create live stream');
             return $success;
         } catch (\Facebook\Exceptions\FacebookSDKException $e) {
             syslog(LOG_NOTICE, "SMH DEBUG : Facebook SDK returned an error: " . $e->getMessage());
