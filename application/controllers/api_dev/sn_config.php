073d0307930befb4f8dfd275369f0c4f548a82d0
root <jmedina616@gmail.com>
2017-06-27 21:00:26 +0000

    latest update

--- application/controllers/api_dev/sn_config.php
+++ application/controllers/api_dev/sn_config.php
@@ -708,4 +708,46 @@ class Sn_config extends REST_Controller {
         $this->response($result, 200); // 200 being the HTTP response code 
     }
 
+    public function update_sn_vod_config_get() {
+        $pid = $this->get('pid');
+        $ks = $this->get('ks');
+        $eid = $this->get('eid');
+        $snConfig = $this->get('snConfig');
+        $projection = $this->get('projection');
+
+        if (!isset($pid) || $pid == null) {
+
+            $this->response(array('error' => 'Missing pid'), 200);
+        }
+
+        if (!isset($ks) || $ks == null) {
+
+            $this->response(array('error' => 'Missing ks'), 200);
+        }
+
+        if (!isset($eid) || $eid == null) {
+
+            $this->response(array('error' => 'Missing eid'), 200);
+        }
+
+        if (!isset($snConfig) || $snConfig == null) {
+
+            $this->response(array('error' => 'Missing snConfig'), 200);
+        }
+
+        if (!isset($projection) || $projection == null) {
+
+            $this->response(array('error' => 'Missing projection'), 200);
+        }
+
+        $result = $this->sn_config_model->update_sn_vod_config($pid, $ks, $eid, $snConfig, $projection);
+
+        if (!$result) {
+
+            $this->response($result, 200);
+        }
+
+        $this->response($result, 200); // 200 being the HTTP response code 
+    }
+
 }
