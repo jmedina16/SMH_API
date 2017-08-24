db8dae69706eff55bf30d297be467f5f0e464aa2
root <jmedina616@gmail.com>
2017-08-24 06:00:25 +0000

    latest update

--- application/controllers/api_dev/sn_config.php
+++ application/controllers/api_dev/sn_config.php
@@ -203,6 +203,30 @@ class Sn_config extends REST_Controller {
         $this->response($result, 200); // 200 being the HTTP response code
     }
 
+    public function remove_twitch_authorization_get() {
+        $pid = $this->get('pid');
+        $ks = $this->get('ks');
+
+        if (!isset($pid) || $pid == null) {
+
+            $this->response(array('error' => 'Missing pid'), 200);
+        }
+
+        if (!isset($ks) || $ks == null) {
+
+            $this->response(array('error' => 'Missing ks'), 200);
+        }
+
+        $result = $this->sn_config_model->remove_twitch_authorization($pid, $ks);
+
+        if (!$result) {
+
+            $this->response($result, 200);
+        }
+
+        $this->response($result, 200); // 200 being the HTTP response code
+    }
+
     public function create_sn_livestreams_get() {
         $pid = $this->get('pid');
         $ks = $this->get('ks');
