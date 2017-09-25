2f68b206d9cb09ec4beda1d7aa1489e9aa816b4e
root <jmedina616@gmail.com>
2017-09-25 21:00:07 +0000

    latest update

---
+++ application/models/channel_dev/channel_config_model.php
@@ -0,0 +1,40 @@
+<?php
+
+error_reporting(1);
+
+class Channel_config_model extends CI_Model {
+
+    protected $_ci;
+
+    public function __construct() {
+        $this->_ci = & get_instance();
+        $this->_ci->load->library("curl");
+        $this->load->library('SMPortal');
+        $this->load->library('SMCipher');
+    }
+
+    public function post_schedule($pid, $ks) {
+        $success = array('success' => false);
+        $live_channels = $this->smportal->get_channels($pid, $ks);
+        syslog(LOG_NOTICE, "SMH DEBUG : post_schedule: " . print_r($live_channels, true));
+
+        return $success;
+    }
+
+    public function verfiy_ks($pid, $ks) {
+        return $this->smportal->verify_ks($pid, $ks);
+    }
+
+    public function verify_service($pid) {
+        $has_service = false;
+        $this->_ci->curl->create("https://mediaplatform.streamingmediahosting.com/apps/services/v1.0/index.php?pid=" . $pid . "&action=get_services");
+        $this->_ci->curl->get();
+        $response = json_decode($this->_ci->curl->execute());
+        if ($response->social_network) {
+            $has_service = true;
+        }
+
+        return $has_service;
+    }
+
+}
