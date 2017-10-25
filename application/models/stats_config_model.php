04f020bcf426b8c5dd0c994a62bce01b4bb56e6a
root <jmedina616@gmail.com>
2017-10-25 21:00:09 +0000

    latest update

--- application/models/stats_config_model.php
+++ application/models/stats_config_model.php
@@ -2,6 +2,8 @@
 
 error_reporting(1);
 
+require_once APPPATH."/libraries/PHPExcel/Classes/PHPExcel.php";
+
 class Stats_config_model extends CI_Model {
 
     protected $_ci;
@@ -19,7 +21,10 @@ class Stats_config_model extends CI_Model {
         $success = array('success' => false);
         $valid = $this->verfiy_ks($pid, $ks);
         if ($valid['success']) {
+            $vodStatsEntries = $this->getVodStats($cpid, $start_date, $end_date);
+            $liveStatsEntries = $this->getLiveStats($cpid, $start_date, $end_date);
             $locationEntries = $this->getLocations($cpid, $start_date, $end_date);
+            $objPHPExcel = new PHPExcel();
         }
 
         return $locationEntries;
@@ -34,8 +39,51 @@ class Stats_config_model extends CI_Model {
 
         $query = $this->config->get();
         $locationEntries = $query->result_array();
-        
+
         return $locationEntries;
+    }
+
+    public function getLiveStats($cpid, $start_date, $end_date) {
+        $this->config->select('*')
+                ->from('content_live_stats')
+                ->where('partner_id', $cpid)
+                ->where('statistics_for >=', $start_date)
+                ->where('statistics_for <=', $end_date);
+
+        $query = $this->config->get();
+        $liveStatsEntries = $query->result_array();
+
+        return $liveStatsEntries;
+    }
+
+    public function getVodStats($cpid, $start_date, $end_date) {
+        $this->config->select('*')
+                ->from('content_vod_stats')
+                ->where('partner_id', $cpid)
+                ->where('statistics_for >=', $start_date)
+                ->where('statistics_for <=', $end_date);
+
+        $query = $this->config->get();
+        $vodStatsEntries = $query->result_array();
+
+        return $vodStatsEntries;
+    }
+
+    public function human_filesize($bytes, $decimals = 2) {
+        $bytes_temp = $bytes;
+        $labels = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        foreach ($labels as $label) {
+            if ($bytes > 1024) {
+                $bytes = $bytes / 1024;
+            } else {
+                break;
+            }
+        }
+
+        $bytes_temp2 = number_format($bytes_temp);
+        $bytes_temp3 = floatval(str_replace(",", ".", $bytes_temp2));
+        return number_format($bytes_temp3, 2) . " " . $label;
     }
 
     public function verfiy_ks($pid, $ks) {
