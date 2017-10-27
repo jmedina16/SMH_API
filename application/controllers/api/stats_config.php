4696c1ad4f384cd247fa67cf38447723a1c0bbbd
root <jmedina616@gmail.com>
2017-10-27 10:00:31 +0000

    latest update

--- application/controllers/api/stats_config.php
+++ application/controllers/api/stats_config.php
@@ -53,4 +53,40 @@ class Stats_config extends REST_Controller {
         $this->response($result, 200); // 200 being the HTTP response code
     }
 
+    public function get_all_child_stats_get() {
+        $pid = $this->get('pid');
+        $ks = $this->get('ks');
+        $start_date = $this->get('start_date');
+        $end_date = $this->get('end_date');
+
+        if (!isset($pid) || $pid == null) {
+
+            $this->response(array('error' => 'Missing pid'), 200);
+        }
+
+        if (!isset($ks) || $ks == null) {
+
+            $this->response(array('error' => 'Missing ks'), 200);
+        }
+
+        if (!isset($start_date) || $start_date == null) {
+
+            $this->response(array('error' => 'Missing start_date'), 200);
+        }
+
+        if (!isset($end_date) || $end_date == null) {
+
+            $this->response(array('error' => 'Missing end_date'), 200);
+        }
+
+        $result = $this->stats_config_model->get_all_child_stats($pid, $ks, $start_date, $end_date);
+
+        if (!$result) {
+
+            $this->response($result, 200);
+        }
+
+        $this->response($result, 200); // 200 being the HTTP response code
+    }
+
 }
