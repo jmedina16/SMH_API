59f5162a746b5980094ef4b543420e7d465e6d98
root <jmedina616@gmail.com>
2018-01-12 03:00:06 +0000

    latest update

--- application/models/channel_dev/channel_config_model.php
+++ application/models/channel_dev/channel_config_model.php
@@ -277,6 +277,64 @@ class Channel_config_model extends CI_Model {
         return $success;
     }
 
+    public function add_program($pid, $ks, $cid, $eid, $start_date, $end_date, $repeat) {
+        $success = array('success' => false);
+        $valid = $this->verfiy_ks($pid, $ks);
+        if ($valid['success']) {
+            $has_service = $this->verify_service($pid);
+            if ($has_service) {
+                $add_live_segment = $this->smportal->add_live_segment($pid, $ks, $cid, $eid);
+                if ($add_live_segment['success']) {
+                    $add_custom_data = $this->update_live_segment_custom_data($pid, $add_live_segment['id'], $start_date, $end_date, $repeat);
+                    if ($add_custom_data['success']) {
+                        $success = array('success' => true);
+                    } else {
+                        $success = array('success' => false, 'message' => 'Could not add custom data');
+                    }
+                } else {
+                    $success = array('success' => false);
+                }
+            } else {
+                $success = array('success' => false, 'message' => 'Channel Manager service not active');
+            }
+        } else {
+            $success = array('success' => false, 'message' => 'Invalid KS: Access Denied');
+        }
+        return $success;
+    }
+
+    public function update_live_segment_custom_data($pid, $sid, $start_date, $end_date, $repeat) {
+        $success = array('success' => false);
+
+        $tz_from = 'America/Los_Angeles';
+        $tz_to = 'UTC';
+        $start_dt = new DateTime($start_date, new DateTimeZone($tz_from));
+        $start_dt->setTimeZone(new DateTimeZone($tz_to));
+        $start_date = $start_dt->format('Y-m-d H:i:s');
+        $end_dt = new DateTime($end_date, new DateTimeZone($tz_from));
+        $end_dt->setTimeZone(new DateTimeZone($tz_to));
+        $end_date = $end_dt->format('Y-m-d H:i:s');
+
+        $segmentConfig = array();
+        $config = array();
+        array_push($config, array('start_date' => $start_date, 'end_date' => $end_date, 'repeat' => $repeat));
+        $segmentConfig['segmentConfig'] = $config;
+        $data = array(
+            'custom_data' => json_encode($segmentConfig)
+        );
+        $this->config = $this->load->database('kaltura', TRUE);
+        $this->config->where('partner_id', $pid);
+        $this->config->where('id', $sid);
+        $this->config->update('live_channel_segment', $data);
+        $this->config->limit(1);
+        if ($this->config->affected_rows() > 0) {
+            $success = array('success' => true);
+        } else {
+            $success = array('success' => false);
+        }
+        return $success;
+    }
+
     public function add_channel($pid, $ks, $eids, $name, $desc) {
         $success = array('success' => false);
         $valid = $this->verfiy_ks($pid, $ks);
@@ -373,28 +431,6 @@ class Channel_config_model extends CI_Model {
             }
         } else {
             $success = array('success' => false, 'message' => 'Invalid KS: Access Denied');
-        }
-        return $success;
-    }
-
-    public function update_live_segment_custom_data($pid, $sid, $sort_value) {
-        $success = array('success' => false);
-        $segmentConfig = array();
-        $config = array();
-        array_push($config, array('sortValue' => $sort_value));
-        $segmentConfig['segmentConfig'] = $config;
-        $data = array(
-            'custom_data' => json_encode($segmentConfig)
-        );
-        $this->config = $this->load->database('kaltura', TRUE);
-        $this->config->where('partner_id', $pid);
-        $this->config->where('id', $sid);
-        $this->config->update('live_channel_segment', $data);
-        $this->config->limit(1);
-        if ($this->config->affected_rows() > 0) {
-            $success = array('success' => true);
-        } else {
-            $success = array('success' => false);
         }
         return $success;
     }
