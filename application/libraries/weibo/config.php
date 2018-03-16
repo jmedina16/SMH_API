efb191901c90ed8edd0ad31d053ff50cbc9a5d55
root <jmedina616@gmail.com>
2018-03-16 00:00:09 +0000

    latest update

---
+++ application/libraries/weibo/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '4097759351' );
+define( "WB_SKEY" , '4558c274031388e0df77d21c3e1de8e0' );
+define( "WB_CALLBACK_URL" , 'https://devplatform.streamingmediahosting.com/apps/sn/v1.0/weibo-callback.php' );
